10cf3235c5ade57b123f52b7d7574d9ea73abdcf
Makayla <makaylabusher@gmail.com>
2022-12-08 12:35:47 -0600

    add update and destroy routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,4 +18,21 @@ class SongsController < ApplicationController
     )
     render json: song.as_json
   end
+
+  def update
+    song = Song.find_by(id:params[:id])
+    song.update(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id:params[:id])
+    song.destroy
+    render json: {message: "This song was deleted!"}
+  end
 end
